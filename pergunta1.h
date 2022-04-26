81bc810a2c8ab1493dd8cded92f9b27935cc1fd6
unknown <juan.bellon2001@hotmail.com>
2022-04-26 19:33:27 +0100

    question 1 almost

---
+++ pergunta1.h
@@ -0,0 +1,10 @@
+#ifndef PERGUNTA1_H
+    #include <stdio.h>
+    #include <string.h>
+    #include <stdlib.h>
+    #define PERGUNTA1_H
+     
+    /*function declaration.*/
+    int peek(FILE *);
+     
+#endif
